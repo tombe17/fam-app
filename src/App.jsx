e0b0a314a433f407a78f16cbaff67c51bfaf85cc
tombe17 <131499102+tombe17@users.noreply.github.com>
2026-02-27 21:15:49 -0700

    Edited app to include buttons for accessing the list of recipes.

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,22 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>The Family Website</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Welcome to the family website! (Still some work to be done.) Currently this page will function as a family cookbook!
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Bellows Cookbook</h1>
+      <div className="button-row">
+        <button className="button">
+          Add Recipe
+        </button>
+        <button className="button">
+          Search Recipes
+        </button>
+        <button className="button">
+          Browse All
+        </button>
+    </div>
     </>
   )
 }
